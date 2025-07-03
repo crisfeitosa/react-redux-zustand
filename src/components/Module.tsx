18737cf2d1cf6e69f19617ef2cd4087b4db52fb4
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-03 15:11:45 -0300

    feat: implement loading state management and improve UI feedback in Header, Video, and Player components

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,10 +1,9 @@
+import { ChevronDown } from "lucide-react";
 import * as Collapsible from '@radix-ui/react-collapsible';
 
-import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
-import { useAppSelector } from "../store";
-import { useDispatch } from "react-redux";
 import { play } from "../store/slices/player";
+import { useAppDispatch, useAppSelector } from "../store";
 
 interface ModuleProps {
   moduleIndex: number;
@@ -13,7 +12,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch();
 
   const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
